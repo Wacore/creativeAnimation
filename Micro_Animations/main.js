45df32df9397f76a86d8ca4f6ef039d1bcbad2ef
Wacore <hoddazheng@gmail.com>
2022-06-06 06:50:44 -0400

    Messages animation has been completed.

--- Micro_Animations/main.js
+++ Micro_Animations/main.js
@@ -1,6 +1,7 @@
 const tl = gsap.timeline({ defaults: { duration: 0.35, ease: "power3.out" } });
 const homeIcon = document.querySelector('.home-svg');
 const notifications = document.querySelector('.notification-svg');
+const messages = document.querySelector('.messages-svg');
 
 tl.set(".feather", { scale: 0, transformOrigin: "center" });
 
@@ -18,4 +19,13 @@ notifications.addEventListener('click', () => {
     tl.fromTo('.bell', { rotation: -5 }, { rotation: 0, duration: 2, ease: "elastic.out(5, 0.2)" });
     tl.fromTo('.ringer', { rotation: 3 }, { rotation: 0, duration: 1, ease: "elastic.out(5, 0.2)" }, "<10%");
     tl.fromTo('.wave', { opacity: 1, scale: 0 }, { opacity: 0, duration: .8, scale: 1.3 }, "<10%")
+})
+
+tl.set(".flap", { transformOrigin: 'top' })
+messages.addEventListener('click', () => {
+    tl.fromTo('.messages-svg', { scale: 1 }, { scale: 0.9 });
+    tl.fromTo('.flap', { scale: 1 }, { scale: -1 }, "<50%");
+    tl.fromTo('.messages-svg', { scale: 0.9 }, { scale: 1 }, "<50%");
+    tl.fromTo('.note', { opacity: 1, y: 0 }, { opacity: 0, y: -40, duration: .7 })
+    tl.to('.flap', { scale: 1 }, "<70%");
 })
